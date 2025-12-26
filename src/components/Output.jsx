c1e1102eaad1712a7a85573f5050227fc8d23a1f
Utsab <utsab1adhikari@gmail.com>
2025-12-26 21:28:29 +0545

    Updated Output component

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -4,7 +4,7 @@ export default function Output({ output }) {
       <h2 className="border-l-4 border-indigo-400 pl-3 text-xl font-semibold text-indigo-900">
         Output:
       </h2>
-      <pre className="overflow-x-auto border border-slate-800 bg-[#020617] px-4 py-3 text-xs text-slate-100">
+      <pre className="overflow-x-auto max-w-[80vw] border border-slate-800 bg-[#020617] px-4 py-3 text-xs text-slate-100">
         <code>{output}</code>
       </pre>
     </section>
